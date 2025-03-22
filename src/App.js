7c71aad4876667468c719eb2c76f268a10b29214
Şefik Emre Akar <akarsefikemre@gmail.com>
2025-03-22 22:33:58 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import UsdtTransfer from './components/UsdtTransfer';
 import './App.css';
 
-// Manifest URL'i process.env üzerinden alıyoruz veya default değer kullanıyoruz
-const manifestUrl = process.env.REACT_APP_MANIFEST_URL || 'https://deneme-tan-psi.vercel.app/tonconnect-manifest.json';
+// Doğru manifest URL'i
+const manifestUrl = 'https://deneme-tan-psi.vercel.app/tonconnect-manifest.json';
 
 function App() {
   return (
